e4d9f19ba88d77a2e099c037f138a1827ebd8acf
archi01-safety <rowoon88@gmail.com>
2026-04-14 17:54:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -323,11 +323,13 @@ with header_col1:
             </a>
         ''', unsafe_allow_html=True)
     else:
-        # 로고 로딩 실패 시 텍스트(영문 약어)로 대체 표출
+
+        # 로고 로딩 실패 시 텍스트(영문 약어 x KYWA)로 대체 표출
         st.markdown(f'''
-            <a href="{inst_url}" target="_blank" 
-               style="text-decoration:none; color:#FF701E; font-weight:bold; font-size:24px; display:block; margin-top:10px;">
-                {inst_abbr}
+            <a href="{inst_url}" target="_blank" style="text-decoration:none; font-weight:bold; font-size:24px; display:block; margin-top:10px;">
+                <span style="color:#FF701E;">{inst_abbr}</span> 
+                <span style="color:#000000;">x</span> 
+                <span style="color:#FF4B4B;">KYWA</span>
             </a>
         ''', unsafe_allow_html=True)
 
@@ -338,8 +340,8 @@ with header_col2:
         </a>
         <p style='color: gray; margin-top: 5px; font-size: 1.05rem;'>
             {cfg['institution']['name']} 스마트 안전관리 플랫폼 
-            <span style='font-size: 0.85rem; letter-spacing: 0.5px; margin-left: 5px;'>
-                Powered by <span style='color: #ff4b4b; font-weight: bold;'>KYWA</span>
+            <span style='font-size: 1rem; letter-spacing: 0.5px; margin-left: 7px;'>
+                Powered by <span style='color: #ff4b4b; font-weight: bold;'>한국청소년활동진흥원(KYWA)</span>
             </span>
         </p>
     """, unsafe_allow_html=True)
